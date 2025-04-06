f14a79aada04533521d055cc99b8622df3382004
akshay-at-bluepineapple <akshay.vishwakarma@bluepineapple.io>
2025-04-06 17:41:37 +0530

    added sh script

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ alembic upgrade head
 
 # Start the FastAPI app with Uvicorn
 echo "Starting FastAPI app..."
-uvicorn main:app --host=0.0.0.0 --port=10000 --reload
+uvicorn TodoApp.main:app --host=0.0.0.0 --port=10000 --reload
