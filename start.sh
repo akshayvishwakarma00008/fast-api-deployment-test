c1720168fa48b07b4f4581a384d0b651d4ed6659
akshay-at-bluepineapple <akshay.vishwakarma@bluepineapple.io>
2025-04-06 17:40:16 +0530

    added sh script

---
+++ start.sh
@@ -0,0 +1,10 @@
+
+#!/bin/bash
+
+# Run Alembic migrations
+echo "Running database migrations..."
+alembic upgrade head
+
+# Start the FastAPI app with Uvicorn
+echo "Starting FastAPI app..."
+uvicorn main:app --host=0.0.0.0 --port=10000 --reload
