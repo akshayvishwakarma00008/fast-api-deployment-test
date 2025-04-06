ba95ff520651c9d3b0201c469d2301e1da68a760
akshay-at-bluepineapple <akshay.vishwakarma@bluepineapple.io>
2025-04-06 17:58:04 +0530

    modified sh script

--- start.sh
+++ start.sh
@@ -2,8 +2,10 @@
 # Run Alembic migrations
 echo "Running database migrations..."
 cd TodoApp
+alembic revision --autogenerate -m "auto migration $(date +%s)"
 alembic upgrade head
 
 # Start the FastAPI app with Uvicorn
 echo "Starting FastAPI app..."
+cd ..
 uvicorn TodoApp.main:app --host=0.0.0.0 --port=10000 --reload
